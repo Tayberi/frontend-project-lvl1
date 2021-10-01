d2f9a50babbbd4a4786ad60087e081a6e903d5bc
Tayberi Evgeny <harmankardon00@gmail.com>
2021-10-01 16:36:02 +0500

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import evenGame from '../src/games/prime.js';
+import run from '../src/engine.js';
+
+run(evenGame);
---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import { generateRandomInt } from '../utils.js';
+
+const isPrime = (num) => {
+    if (num < 2) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const description = 'Answer "yes" if the number is prime, otherwise answer "no".';
+const makeRound = () => {
+    const number = generateRandomInt(1, 23);
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    const question = number.toString();
+
+    return { correctAnswer, question };
+};
+
+export default { description, makeRound };
