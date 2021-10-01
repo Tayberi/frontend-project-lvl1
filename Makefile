6bd27c3909706edf2c39498c32fab2f633a37e3a
Tayberi Evgeny <harmankardon00@gmail.com>
2021-10-01 16:13:16 +0500

    add brain-gcd

--- Makefile
+++ Makefile
@@ -13,6 +13,8 @@ brain-even:
 brain-calc:
 	node bin/brain-calc.js
 
+brain-gcd:
+	node bin/brain-gcd.js
 
 lint:
 	npx eslint .
---
+++ src/games/gcd.js
@@ -0,0 +1,15 @@
+import { generateRandomInt } from '../utils.js';
+
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+const description = 'Find the greatest common divisor of given numbers.';
+const makeRound = () => {
+    const number1 = generateRandomInt(1, 30);
+    const number2 = generateRandomInt(1, 30);
+    const correctAnswer = gcd(number1, number2).toString();
+    const question = `${number1} ${number2}`;
+
+    return { correctAnswer, question };
+};
+
+export default { description, makeRound };
